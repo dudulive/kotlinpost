cc46584d654132f0aeac60acdfb733e1d4367318
dudulive <edurodriguesbraz@hotmail.com>
2018-05-07 23:25:53 -0300

    Criando pacotes auxiliares.

---
+++ src/main/kotlin/entity/postEntity.kt
@@ -0,0 +1,8 @@
+package entity
+
+data class PostEntity(
+    val id: Int,
+    val userId: Int,
+    val title: String ,
+    val body: String
+)
---
+++ src/main/kotlin/infra/endpointConstants.kt
@@ -0,0 +1,14 @@
+package infra
+
+class EndpointConstansts (){
+
+    object BASE  {
+        val URL : String = "https://jsonplaceholder.typicode.com/"
+    }
+
+    object POST  {
+        val ALL_POST : String = "post"
+        val SINGLE_POST : String = "post"
+    }
+
+}
