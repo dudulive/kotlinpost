41444280570380446ae6ea96a7f6cb24f7243d30
dudulive <edurodriguesbraz@hotmail.com>
2018-05-10 23:26:26 -0300

    Convertendo dados da API

--- src/main/kotlin/business/postBusiness.kt
+++ src/main/kotlin/business/postBusiness.kt
@@ -1,26 +1,29 @@
 package business
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import entity.FullParameters
 import entity.HttpResponse
+import entity.PostEntity
 import infra.EndpointConstansts
 import infra.OperationMethod
 import repository.PostRepository
 
 class PostBusiness(){
 
-    fun getAllPosts(): HttpResponse {
+    fun getAllPosts(): List<PostEntity> {
         val url = EndpointConstansts.BASE.URL + EndpointConstansts.POST.ALL_POSTS
         val fullParameters = FullParameters(url, OperationMethod.GET)
         val response: HttpResponse = PostRepository.getAllPosts(fullParameters);
 
-        return response
+        return Gson().fromJson<List<PostEntity>>(response.jsonResponse, object : TypeToken<List<PostEntity>>() {}.type)
     }
 
-    fun getSinglePost(id: Int): HttpResponse {
+    fun getSinglePost(id: Int): PostEntity {
         val url = EndpointConstansts.BASE.URL + EndpointConstansts.POST.SINGLE_POSTS
         val fullParameters = FullParameters(url, OperationMethod.GET, mapOf(Pair("id", id.toString())))
         val response: HttpResponse = PostRepository.getSinglePost(fullParameters);
 
-        return response
+        return Gson().fromJson<List<PostEntity>>(response.jsonResponse, object : TypeToken<List<PostEntity>>() {}.type)[0]
     }
 }
